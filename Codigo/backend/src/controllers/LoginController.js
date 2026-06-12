b2fef953f9bc9ebe66c55b1fc70043cf40fec841
Guilherme Antonio Santos de Oliveira <guilhermegaso420@gmail.com>
2026-06-12 20:40:49 -0300

    Desenvolvendo LoginController

--- Codigo/backend/src/controllers/LoginController.js
+++ Codigo/backend/src/controllers/LoginController.js
@@ -4,6 +4,27 @@ import { body } from 'express-validator';
 
 export const LoginValidators = {
     create: [
-        
+        body('email').isEmail().withMessage('E-mail inválido'),
+        body('senha').notEmpty().withMessage('A senha é obrigatória')
     ]
+}
+
+export const createLoginController = async (req, res) => {
+    try {
+        const { email, senha } = req.body;
+
+        const usuario = await models.userModel.findOne({
+            where: {email}
+        });
+
+        if (!usuario) {
+            return res.status(401).json({ error: 'E-mail ou senha incorretos.' });
+        }
+
+        const senhaHasheada = crypto.createHash('sha256').update(senha).digest('hex');
+
+    } catch(error) {
+        console.error('Erro no LoginController: ', error);
+        return res.status(500).json({ error: 'Erro interno ao processar login.'});
+    }
 }
