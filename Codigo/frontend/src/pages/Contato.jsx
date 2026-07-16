dbcf9a5d41e2a4534931a206d7bec5977b61e690
Guilherme Antonio Santos de Oliveira <guilhermegaso420@gmail.com>
2026-07-15 21:42:39 -0300

    Implementando parte do front e service

--- Codigo/frontend/src/pages/Contato.jsx
+++ Codigo/frontend/src/pages/Contato.jsx
@@ -6,6 +6,7 @@ import FormInput from '../components/FormInput'
 import Button from '../components/Button'
 import Card from '../components/Card'
 import styles from '../styles/Contato.module.css'
+import { contato } from '../services/ContatoService'
 
 export default function Contato() {
   const [formData, setFormData] = useState({
@@ -59,16 +60,24 @@ export default function Contato() {
 
     if (Object.keys(newErrors).length === 0) {
       setIsLoading(true)
+
+      setSubmitSuccess(true)
+      const envio = await contato(formData);
+      setIsLoading(false)
+      
+      setTimeout(() => setSubmitSuccess(false), 5000)
       // Simular envio de dados
-      setTimeout(() => {
-        console.log('Contato:', formData)
-        setIsLoading(false)
-        setSubmitSuccess(true)
-        setFormData({ nome: '', email: '', assunto: '', mensagem: '' })
+      // setTimeout(() => {
+      //   console.log('Contato:', formData)
+      //   setIsLoading(false)
+      //   setFormData({ nome: '', email: '', assunto: '', mensagem: '' })
         
-        // Limpar mensagem de sucesso após 5 segundos
-        setTimeout(() => setSubmitSuccess(false), 5000)
-      }, 1000)
+
+      //   setSubmitSuccess(true)
+      //   // Limpar mensagem de sucesso após 5 segundos
+      //   setTimeout(() => setSubmitSuccess(false), 5000)
+      // }, 1000)
+
     } else {
       setErrors(newErrors)
     }
