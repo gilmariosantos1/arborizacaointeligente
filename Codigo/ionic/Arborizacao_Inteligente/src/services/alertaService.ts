825c775975dae65c126bb741afe6fb8ca2078af2
Breno <nobresilva360@gmail.com>
2026-08-04 20:18:48 -0300

    .

--- Codigo/ionic/Arborizacao_Inteligente/src/services/alertaService.ts
+++ Codigo/ionic/Arborizacao_Inteligente/src/services/alertaService.ts
@@ -1,10 +1,6 @@
 import api from './api';
 
 export const createAlerta = (formData: FormData) =>
-  api.post('/alertas', formData, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  });
+  api.post('/alertas', formData);
 
 export const listAlertas = () => api.get('/alertas');
